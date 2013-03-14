1b460a118b2a136e3b6a3ad04c7d833a4fd09035
Mike Anderson <athaeryn@me.com>
2013-03-14 13:04:53 -0500

    parseData now returns appropriate values. Also made regex for remove the square brackets global so as not to miss any.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,23 +1,20 @@
 $(document).ready(function () {
 
-
     function parseData(data) {
         if (data[0] !== '[') {
-            return
+            return "Error.";
         } else {
             data = data.split("],["); 
             for (var i = 0; i < data.length; i++) {
-                data[i] = data[i].replace(/[\[|\]]*/, ''); 
+                data[i] = data[i].replace(/[\[|\]]/g, ''); 
                 data[i] = data[i].split(',');
-                var date = new Date(data[i][0]);
-                console.log(date);
             }
-            console.log(data);
         }
+        return data;
     }
 
     $.get('getData.php?user=athaeryn', function (d){
-        parseData(d);
+        console.log(parseData(d));
     });
 
 
@@ -28,6 +25,7 @@ $(document).ready(function () {
 
     paper.rect(0, 0, 634, 82).attr({fill: "#fff", stroke: "none"});
 
-    paper.rect(0, 0, 10, 10).attr({fill: "#eee", stroke: "none"});
-    paper.rect(12, 0, 10, 10).attr({fill: "#eee", stroke: "none"});
+    var boxStyle = {fill: "#eee", stroke: "none"};
+    paper.rect(0, 0, 10, 10).attr(boxStyle);
+    paper.rect(12, 0, 10, 10).attr(boxStyle);
 });
