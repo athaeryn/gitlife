afee91e44f7599628ee00f9be3b5978c9d01e5e4
Mike Anderson <athaeryn@me.com>
2013-03-14 10:56:07 -0500

    We can now grab the calendar data.

---
+++ public_html/getData.php
@@ -0,0 +1,27 @@
+<?php
+
+if (!isset($_GET['user'])) {
+    echo "Error: no user specified.";
+} else {
+    $user = $_GET['user'];
+    $url = 'https://github.com/users/'.$user.'/contributions_calendar_data';
+    $handle = curl_init($url);
+    curl_setopt($handle,  CURLOPT_RETURNTRANSFER, TRUE);
+
+    /* Get the HTML or whatever is linked in $url. */
+    $response = curl_exec($handle);
+
+    /* Check for 404 (file not found). */
+    $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+    if($httpCode == 404) {
+        /* Handle 404 here. */
+        die ('invalid user');
+    }
+
+    curl_close($handle);
+
+    echo $response;
+
+}
+
+?>
--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,26 @@
 $(document).ready(function () {
 
+
+    function parseData(data) {
+        if (data[0] !== '[') {
+            return
+        } else {
+            data = data.split("],["); 
+            for (var i = 0; i < data.length; i++) {
+                data[i] = data[i].replace(/[\[|\]]*/, ''); 
+                data[i] = data[i].split(',');
+                var date = new Date(data[i][0]);
+                console.log(date);
+            }
+            console.log(data);
+        }
+    }
+
+    $.get('getData.php?user=athaeryn', function (d){
+        parseData(d);
+    });
+
+
     var paper = new Raphael(document.getElementById("grid"), 634, 82),
         w = 10,
         h = 10,
