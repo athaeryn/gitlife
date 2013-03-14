c94b8289511540058a01fcbc66ab82f9f9a374a8
Mike Anderson <athaeryn@me.com>
2013-03-14 13:05:19 -0500

    Looks much more like Github now.

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,8 @@
 <?php include "header.php" ?>
 <div class="page-contain">
-<h1>gitlife</h1>
-<div id="grid"></div>
+    <div class="grid-container">
+        <h3>gitlife</h3>
+        <div id="grid"></div>
+    </div>
 </div>
 <?php include "footer.php" ?>
