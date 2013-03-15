a57dfce7d4919aa2be0a5e7e1fd2ad87cd667f45
Mike Anderson <athaeryn@me.com>
2013-03-14 19:36:48 -0500

    Added title and favicon.

--- public_html/header.php
+++ public_html/header.php
@@ -6,10 +6,11 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
+        <title>-Gitlife-</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
 
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
         <link rel="stylesheet" href="css/main.css">
 
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
@@ -18,5 +19,4 @@
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
-
 
