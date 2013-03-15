0bb0804ee9d02d7e8597d2e6fd51f338728e6a64
Mike Anderson <athaeryn@me.com>
2013-03-15 15:57:08 -0500

    Data is loaded, grid is populated.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,59 +1,72 @@
 $(document).ready(function () {
-
-    function parseData(data) {
-        if (data[0] !== '[') {
-            return "Error.";
-        } else {
-            data = data.split("],["); 
-            for (var i = 0; i < data.length; i++) {
-                data[i] = data[i].replace(/[\[|\]|"]/g, ''); 
-                data[i] = data[i].split(',');
-            }
-        }
-        
-        important = [];
-        important[0] = -(new Date(data[0][0]).getDay());
-
-        for (var i = 0; i < data.length; i++) {
-            if (data[i][1] > 0) {
-                //important.push(data[i][0]);
-                important.push(i);
-            }
-        }
-
-        return important;
-    }
-
-    var paper = new Raphael(document.getElementById("grid"), 634, 82),
+    // Global stuff
+    var paper,
+        data,
+        startingDay,
         cw = 10,
         ch = 10,
         cpad = 2,
         w = 53,
         h = 7,
-        dead = {fill: "#eee", stroke: "none"};
-        alive = {fill: "#1E6822", stroke: "none"};
+        deadStyle = {fill: "#eee", stroke: "none"};
+        aliveStyle = {fill: "#1E6822", stroke: "none"};
 
-    paper.rect(0, 0, 634, 82).attr({fill: "#fff", stroke: "none"});
+    function parseData(raw) {
+        var parsed = [];
 
-    function drawCell(x, y, state) {
+        //  Just throw an error if the data is not valid.
+        //  It should start with '['
+        if (raw[0] !== '[') return "Error.";
+
+        // Parse the data from wierdness format to something workable
+        raw = raw.split("],["); 
+        for (var i = 0; i < raw.length; i++) {
+            raw[i] = raw[i].replace(/[\[|\]|"]/g, '').split(',');
+        }
+       
+        // Grab the day of the week of the first data point
+        startingDay = new Date(raw[0][0]).getDay();
+        
+        // Push the commit counts onto the 'parsed' array 
+        for (var i = 0; i < raw.length; i++) {
+            parsed.push(raw[i][1] > 0);
+        }
+
+        return parsed;
+    }
+   
+    // Initalize the canvas and draw the empty grid
+    paper = new Raphael(document.getElementById("grid"), 634, 82);
+    drawEmptyGrid();
+
+    // Draws a cell 
+    function drawCell(x, y, alive) {
         var cell = paper.rect(x * (cw + cpad), y * (ch + cpad), cw, ch);
-        cell.attr(state > 0 ? alive : dead);
+        cell.attr(alive ? aliveStyle : deadStyle);
     }
 
-    function drawGrid(data) {
-        var first = -data[0];
-        console.log(first);
-        for (var x = 0; x < 53; x++) {
-            var yStart = x === 0 ? first : 0;
-            var yEnd = x === 52 ? 7 - (5 - first) : 7;
-            for (var y = yStart; y < yEnd; y++) {
-                drawCell(x, y, 0);
+    // Draws the grid with the cell states pulled from data
+    function drawGrid() {
+        for (var x = 0; x < w; x++) {
+            for (var y = 0; y < h; y++) {
+                drawCell(x, y, data[(x - 1) * h + y + (7 - startingDay)]);
             }
         }
     }
 
+    // Draws the grid with all the cells "off"
+    function drawEmptyGrid() {
+        for (var x = 0; x < w; x++) {
+            for (var y = 0; y < h; y++) {
+                drawCell(x, y, false);
+            }
+        }
+    }
+
+    // Load the data, parse it, and draw the grid
     $.get('getData.php?user=athaeryn', function (d){
-        console.log(parseData(d));
-        drawGrid(parseData(d));
+        data = parseData(d);
+        console.log(data);
+        drawGrid();
     });
 });
