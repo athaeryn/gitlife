8218a98c1a87ffcfad53c23abc3cb670197ab0db
Mike Anderson <athaeryn@me.com>
2013-03-20 12:21:52 -0500

    Remove plugins.js.

--- public_html/footer.php
+++ public_html/footer.php
@@ -2,7 +2,6 @@
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
 
         <script src="js/raphael.js"></script>
-        <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
 
         <script>
