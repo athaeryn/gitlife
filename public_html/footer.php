382074fdb391b02a81bbfb8750fa8c35087ab70b
Mike Anderson <athaeryn@me.com>
2013-03-20 23:19:03 -0500

    Add Gauges tracking code.

--- public_html/footer.php
+++ public_html/footer.php
@@ -10,5 +10,19 @@
             g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
             s.parentNode.insertBefore(g,s)}(document,'script'));
         </script>
+
+        <script type="text/javascript">
+            var _gauges = _gauges || [];
+            (function() {
+                var t   = document.createElement('script');
+                t.type  = 'text/javascript';
+                t.async = true;
+                t.id    = 'gauges-tracker';
+                t.setAttribute('data-site-id', '514a89a9613f5d208f000155');
+                t.src = '//secure.gaug.es/track.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(t, s);
+            })();
+        </script>
     </body>
 </html>
