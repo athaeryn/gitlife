e56b8a3b9d58f34c4cb031a2b61afdc582e1ad72
Mike Anderson <athaeryn@me.com>
2013-03-31 03:08:21 -0500

    Simulation advances correctly.

--- public_html/js/Solver.js
+++ public_html/js/Solver.js
@@ -1,18 +1,95 @@
-/*global validateArgs */
+/*global validateArgs, alert */
 
 function Solver(args) {
     "use strict";
     args = args || {};
     var data,
-        tempData,
+        backData,
         steps = 0,
-        initialized;
+        initialized,
+        advance,
+        bake,
+        w,
+        h;
 
     validateArgs(args, ["height", "width", "data"], "Solver");
 
+    data = args.data;
+    w = args.width;
+    h = args.height;
+
+    if (data.length && data.length !== w * h) {
+        throw new Error("Data size and dimensions do not match.");
+    }
+
+    // bake the neighbors
+    (function () {
+        var i,
+            x,
+            xOffset,
+            y,
+            yOffset,
+            neighbors;
+        bake = [];
+        for (i = 0; i < data.length; i += 1) {
+            /* How this is supposed to work:
+             *  create an object which acts like a hash
+             *  each key will be an index of the data array
+             *  the value of the key will be an array of the positions
+             *  of the key's neighbors
+             * */
+            neighbors = [];
+            for (x = 0; x < w; x += 1) {
+                for (y = 0; y < h; y += 1) {
+                    neighbors = [];
+                    for (xOffset = -1; xOffset < 2; xOffset += 1) {
+                        for (yOffset = -1; yOffset < 2; yOffset += 1) {
+                            if (!(xOffset === 0 && yOffset === 0)) {
+                                neighbors.push(
+                                    ((x + w + xOffset) % w) * h +
+                                        ((y + h + yOffset) % h)
+                                );
+                            }
+                        }
+                    }
+                    bake.push(neighbors);
+                }
+            }
+        }
+        return bake;
+    }());
+
+    function solveCell(i) {
+        var liveNeighborCount = 0,
+            currentState = data[i],
+            neighbors = bake[i],
+            newState,
+            j;
+        for (j = 0; j < neighbors.length; j += 1) {
+            liveNeighborCount += data[neighbors[j]];
+        }
+        if (liveNeighborCount === 3 || (liveNeighborCount === 2 && currentState)) {
+            newState = true;
+        } else {
+            newState = false;
+        }
+        return newState;
+    }
+
+    advance = function () {
+        var cell;
+        backData = [];
+        for (cell = 0; cell < data.length; cell += 1) {
+            backData.push(solveCell(cell));
+        }
+        data = backData;
+        steps += 1;
+    };
+
     return {
         step: function () {
-            //TODO return data, and steps
+            advance();
+            return {data: data, steps: steps};
         }
     };
 }
--- testing/spec/SolverSpec.js
+++ testing/spec/SolverSpec.js
@@ -1,8 +1,37 @@
 /*global describe, it, expect, Solver */
 describe("The Solver", function () {
     "use strict";
-    it("should throw an error when not initialized with a height, width, and data.", function () {
+    it("should throw an error when not initialized with the right properties.", function () {
         var expectedError = "Missing argument(s) for Solver: height,width,data";
         expect(function () { var derp = new Solver(); }).toThrow(new Error(expectedError));
+    });
+    it("should be able to correctly advance the simulation.", function () {
+        var input = [
+                false, false, false, false, false,
+                false, false, false, false, false,
+                false, true, true, true, false,
+                false, false, false, false, false,
+                false, false, false, false, false],
+            expected = {
+                data: [
+                    false, false, false, false, false,
+                    false, false, true, false, false,
+                    false, false, true, false, false,
+                    false, false, true, false, false,
+                    false, false, false, false, false],
+                steps: 1
+            },
+            solver = new Solver({width: 5, height: 5, data: input}),
+            output = solver.step();
+        expect(output).toEqual(expected);
+    });
+    it("should throw an error if the data's length does not match the given dimensions.", function () {
+        var expectedError = "Data size and dimensions do not match.";
+        expect(function () {var z = new Solver({
+            width: 10,
+            height: 10,
+            data: [0, 0, 0]
+        }); }).toThrow(new Error(expectedError));
+
     });
 });
