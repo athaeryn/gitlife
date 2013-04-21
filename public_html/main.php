33975ff708d0e690da6d90f2da6d7152db916a0d
Mike Anderson <athaeryn@me.com>
2013-04-21 02:24:59 -0500

    Beginning new design.

--- public_html/main.php
+++ public_html/main.php
@@ -1,21 +1,20 @@
-        <div class="page-contain">
-            <div id="left-column">
+        <div class="page-inner">
+            <div class="left-column">
                 <img src="http://gravatar.com/avatar/a4db33172b6f59a50b7e9621538026ed?s=210" id="gravatar" height="210" width="210">
                 <h1 class="name">Mike Anderson</h1>
                 <h3 class="username">athaeryn</h3>
                 <hr>
             </div>
-            <div class="grid-container">
-                <h3 class="clearfix"><span id="stepsBox">--</span></h3>
-                <canvas id="grid" width="634" height="250">
-                    This website uses HTML5's canvas to function. If you're reading 
-                    this, your browser doesn't support canvas, which means you're 
-                    probably running an old version of IE. Consider updating that old 
-                    thing so you can join in the fun!
-                </canvas>
-            </div>
-            <div class="footer">
-                by <a href="http://mrmikea.com">Mike Anderson</a>
+            <div class="right-column">
+                <div class="grid-container">
+                    <h3 class="clearfix"><span id="stepsBox">--</span></h3>
+                    <canvas id="grid" width="634" height="250">
+                        This website uses HTML5's canvas to function. If you're reading 
+                        this, your browser doesn't support canvas, which means you're 
+                        probably running an old version of IE. Consider updating that old 
+                        thing so you can join in the fun!
+                    </canvas>
+                </div>
             </div>
         </div>
 
