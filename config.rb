2ebedf4e00e493fe4cf3099fe24ffef3857eeb10
Mike Anderson <athaeryn@me.com>
2013-04-28 15:41:06 -0500

    Set up Compass.

---
+++ config.rb
@@ -0,0 +1,7 @@
+http_path = "/"
+css_dir = "public_html/css"
+sass_dir = "public_html/css/sass"
+images_dir = "public_html/img"
+javascripts_dir = "public_html/js"
+
+sass_options = { :debug_info => true}
